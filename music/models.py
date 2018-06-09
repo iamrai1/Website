fdb3e069329676f6a1acd66b2b87cf21654fd095
iamrai1 <rr05041997@gmail.com>
2018-06-09 22:36:38 +0530

    added user authentication and login

--- music/models.py
+++ music/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 # Create your models here.
 class Album(models.Model):
 
-    artist = models.CharField(max_length= 250)
+    artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     album_logo = models.FileField()
