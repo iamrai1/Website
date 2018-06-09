792c507e4aaa7ad11689ff700a5150be179c082c
iamrai1 <rr05041997@gmail.com>
2018-06-09 10:14:26 +0530

    added favorite/unfavorite option to songs

--- music/models.py
+++ music/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Album(models.Model):
-
     artist = models.CharField(max_length= 250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
@@ -16,6 +15,7 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    is_favorite = models.BooleanField(default= False)
 
     def __str__(self):
         return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 def index(request):
     all_albums = Album.objects.all()
@@ -8,5 +8,18 @@ def index(request):
 
 def detail(request, album_id):
     album = get_object_or_404(Album, pk = album_id)
-
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk = request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {'album': album, 'error_message' : " Please select a valid song"})
+    else:
+        if selected_song.is_favorite:
+            selected_song.is_favorite = False
+        else:
+            selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
